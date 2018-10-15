df94701e39b108abf07deed067565771f7a807c4
Nik Kalyani <nik@kalyani.com>
2018-10-15 11:24:48 -0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>I am looking forward to the JAMstack Hackathon</p>
+    <p>More about me <a href="https://linkedin.com/in/techbubble">here</a></p>
+  </Layout>
+)
+
+export default IndexPage
